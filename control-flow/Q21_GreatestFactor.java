6343423eafcd40b2fd9500a425bba582e3b1e13f
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-13 10:28:27 +0530

    [Aditya Patel] Add greatest factor of a number

--- control-flow/Q21_GreatestFactor.java
+++ control-flow/Q21_GreatestFactor.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q21_GreatestFactor {
+public class GreatestFactor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), greatestFactor = 1;
