e7b7b64d27fae8a441dc9b1cc6476036c7056517
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-13 10:26:54 +0530

    [Aditya Patel] Add sum of natural numbers

--- control-flow/Q1_SumNaturalNumbers.java
+++ control-flow/Q1_SumNaturalNumbers.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q1_SumNaturalNumbers {
+public class SumNaturalNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
