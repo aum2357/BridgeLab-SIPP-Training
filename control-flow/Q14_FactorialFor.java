ba158226e0046f8ee81c9338d0b6d1581205a693
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-13 10:09:05 +0530

    find a factorial of a number

--- control-flow/Q14_FactorialFor.java
+++ control-flow/Q14_FactorialFor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q14_FactorialFor {
-    public static void main(String[] args) {
+    public static void main(String...rgs) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt(), factorial = 1;
         if (num >= 0) {
