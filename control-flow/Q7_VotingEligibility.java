bce50e15889d90c6294163268c4751ab4e3e93d7
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-13 10:46:28 +0530

    [Aditya Patel] Add voting eligibility by given age

--- control-flow/Q7_VotingEligibility.java
+++ control-flow/Q7_VotingEligibility.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q7_VotingEligibility {
+public class VotingEligibility {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
