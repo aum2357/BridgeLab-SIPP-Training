314112a2515a9ef9f58a3318cdcd668c9f0c3e0a
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-13 10:04:42 +0530

    do sum until we get zero

--- control-flow/Q12_SumUntilZeroBreak.java
+++ control-flow/Q12_SumUntilZeroBreak.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q12_SumUntilZeroBreak {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         double total = 0.0;
         while (true) {
