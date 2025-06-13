c65e0c2433be46ebd5d1f6d8789a0c13b2f78ba2
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-13 10:45:21 +0530

    [Aditya Patel] Add find sum until zero

--- control-flow/Q5_SumUntilZero.java
+++ control-flow/Q5_SumUntilZero.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q5_SumUntilZero {
+public class SumUntilZero {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double total = 0.0, num;
