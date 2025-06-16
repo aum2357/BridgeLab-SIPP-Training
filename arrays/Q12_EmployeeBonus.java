35873166893cada8d519821253c1a390b752df69
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:12:05 +0530

    [Aditya Patel] Add employee bonus calculator

--- arrays/Q12_EmployeeBonus.java
+++ arrays/Q12_EmployeeBonus.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q12_EmployeeBonus {
+public class EmployeeBonus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[] salaries = new double[10];
