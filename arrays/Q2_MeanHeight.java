eed90826b1eabbbbc1fde9b72fb46008c1979eb5
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:19:39 +0530

    [Aditya Patel] Add mean height finder

--- arrays/Q2_MeanHeight.java
+++ arrays/Q2_MeanHeight.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q2_MeanHeight {
+public class MeanHeight {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double[] heights = new double[11];
