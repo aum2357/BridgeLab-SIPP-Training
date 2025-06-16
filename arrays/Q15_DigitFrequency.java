efb7f7ac3323068042816cf4d5779cba3b317efe
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:14:20 +0530

    [Aditya Patel] Add find digit frequency

--- arrays/Q15_DigitFrequency.java
+++ arrays/Q15_DigitFrequency.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q15_DigitFrequency {
+public class DigitFrequency {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
