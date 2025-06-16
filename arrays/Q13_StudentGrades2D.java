3e9c35e82a9f4e583b381067db583633dd6a4583
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:12:47 +0530

    [Aditya Patel] Add student grade finder

--- arrays/Q13_StudentGrades2D.java
+++ arrays/Q13_StudentGrades2D.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q13_StudentGrades2D {
+public class StudentGrades2D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of students: ");
