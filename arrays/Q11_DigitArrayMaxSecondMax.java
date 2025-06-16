1a2f6d262be78b15059d36a5b7b4134ed22ecf6e
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:11:19 +0530

    [Aditya Patel] Add find max and second max digit

--- arrays/Q11_DigitArrayMaxSecondMax.java
+++ arrays/Q11_DigitArrayMaxSecondMax.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q11_DigitArrayMaxSecondMax {
+public class DigitArrayMaxSecondMax {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
