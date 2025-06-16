3da9b33bec9c3d71a2ec3f51b5fbcd1f32f35bc7
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:15:06 +0530

    [Aditya Patyel] Add bmi printer with 1d array

--- arrays/Q16_BMIArray.java
+++ arrays/Q16_BMIArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q16_BMIArray {
+public class BMIArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of persons: ");
