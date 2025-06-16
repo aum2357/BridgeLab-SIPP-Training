9f9f4998c2552f443fba45ca1eeac5c06275bb85
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:18:09 +0530

    [Aditya Patel] Add copy 2d array to 1d array

--- arrays/Q1_Copy2DTo1D.java
+++ arrays/Q1_Copy2DTo1D.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q1_Copy2DTo1D {
+public class Copy2DTo1D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of rows: ");
