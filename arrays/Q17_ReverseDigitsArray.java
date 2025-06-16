9779098d5c0f3526b3de1bfdca7e27efa4bee815
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:15:54 +0530

    [Aditya Patel] Add reverse the array digits

--- arrays/Q17_ReverseDigitsArray.java
+++ arrays/Q17_ReverseDigitsArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q17_ReverseDigitsArray {
+public class ReverseDigitsArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
