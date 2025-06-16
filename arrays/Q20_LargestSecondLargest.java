5d90250d22480bdc2c36d81b01ddf0f214ad1312
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:19:02 +0530

    [Aditya Patel] Add largest and second largest number finder

--- arrays/Q20_LargestSecondLargest.java
+++ arrays/Q20_LargestSecondLargest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q20_LargestSecondLargest {
+public class LargestSecondLargest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
