074ae8202ad53c7fb364920e53b3507a9504a827
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-16 10:23:37 +0530

    [Aditya Patel] Add print fizz and buzz

--- arrays/Q7_FizzBuzz.java
+++ arrays/Q7_FizzBuzz.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q7_FizzBuzz {
+public class FizzBuzz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a positive number: ");
