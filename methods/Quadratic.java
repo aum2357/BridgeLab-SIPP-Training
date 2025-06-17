c32cc61a88a49062e5b19cc306198fdc71f04a34
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:52:53 +0530

    [Aditya Patel] Add root finder of square equation

--- methods/Quadratic.java
+++ methods/Quadratic.java
@@ -10,7 +10,7 @@ public class Quadratic {
         return new double[]{root1, root2};
     }
 
-    public static void main(String[] args) {
+    public static void main(String...args) {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
