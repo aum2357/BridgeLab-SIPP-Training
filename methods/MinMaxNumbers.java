ef94968c029bc2e8c8380001e56b60a1eedb7071
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:46:31 +0530

    [Aditya Patel] Add find min and max number

--- methods/MinMaxNumbers.java
+++ methods/MinMaxNumbers.java
@@ -8,7 +8,7 @@ public class MinMaxNumbers {
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
