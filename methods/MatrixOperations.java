8b7bebe386bd106e8bfcebaf3846b7f8f1517921
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:45:20 +0530

    [Aditya Patel] Add matrix operations

--- methods/MatrixOperations.java
+++ methods/MatrixOperations.java
@@ -49,7 +49,7 @@ public class MatrixOperations {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int rows = scanner.nextInt();
         int cols = scanner.nextInt();
