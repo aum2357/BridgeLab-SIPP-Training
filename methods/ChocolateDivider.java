a143f60439f36cc2237a51f7ba9d2f9337cae061
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:35:46 +0530

    [Aditya Patel] Add chocolate divider among childrens

--- methods/ChocolateDivider.java
+++ methods/ChocolateDivider.java
@@ -8,7 +8,7 @@ public class ChocolateDivider {
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String...args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfChocolates = scanner.nextInt();
         int numberOfChildren = scanner.nextInt();
