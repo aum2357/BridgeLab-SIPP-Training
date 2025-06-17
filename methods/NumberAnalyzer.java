adf1f99ad90de3c01765d5cb06e84b225ee86a16
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:47:27 +0530

    [Aditya Patel] Add number analyzer

--- methods/NumberAnalyzer.java
+++ methods/NumberAnalyzer.java
@@ -15,7 +15,7 @@ public class NumberAnalyzer {
         return 0;
     }
 
-    public static void main(String[] args) {
+    public static void main(String...args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[5];
         for (int i = 0; i < 5; i++) {
