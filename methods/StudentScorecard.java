6af2f8cf1264a7cf18e0a39be91de27abe871f64
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:56:18 +0530

    [Aditya Patel] Add student record printer

--- methods/StudentScorecard.java
+++ methods/StudentScorecard.java
@@ -41,7 +41,7 @@ public class StudentScorecard {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int numStudents = scanner.nextInt();
         int[][] scores = generateScores(numStudents);
