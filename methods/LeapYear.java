733bf31d4c4f2c7c95124120b6fd5d726f902b58
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:43:51 +0530

    [Aditya Patel] Add is year leqap or not

--- methods/LeapYear.java
+++ methods/LeapYear.java
@@ -6,7 +6,7 @@ public class LeapYear {
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
         System.out.println(isLeapYear(year) ? "Leap Year" : "Not a Leap Year");
