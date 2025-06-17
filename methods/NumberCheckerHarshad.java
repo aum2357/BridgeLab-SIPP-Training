d585e566e84bb79790da56826d6d6e495d6d8afb
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-17 10:49:58 +0530

    [Aditya Patel] Add harashed number checker

--- methods/NumberCheckerHarshad.java
+++ methods/NumberCheckerHarshad.java
@@ -46,7 +46,7 @@ public class NumberCheckerHarshad {
         return frequency;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         System.out.println("Digit count: " + countDigits(number));
