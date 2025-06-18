eef4611d77286792202b0203b4d3ca23bfcc35a8
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-18 10:17:45 +0530

    [Aditya Patel ] Add string length finder

--- Strings/Q15_StringLengthManual.java
+++ Strings/Q15_StringLengthManual.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q15_StringLengthManual {
+public class StringLengthManual {
     // Method to find length without using length()
     public static int getLength(String str) {
         int count = 0;
