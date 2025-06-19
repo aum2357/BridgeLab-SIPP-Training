5bd7b4849a9b25ffbda6349c7d529aff0a489362
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-19 11:33:10 +0530

    [Aditya Patel] Add number guessing game

--- builtin_funs_prac/NumberGuessingGame.java
+++ builtin_funs_prac/NumberGuessingGame.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 import java.util.Random;
+
 public class NumberGuessingGame {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-        int low = 1, high = 100;
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1; // Generates number between 1 and 100
+        int guess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 100. Can you guess it?");
+
         while (true) {
-            int guess = (low + high) / 2;
-            System.out.println("Is your number " + guess + "? (high/low/correct)");
-            String feedback = sc.next();
-            if (feedback.equals("correct")) break;
-            else if (feedback.equals("high")) high = guess - 1;
-            else if (feedback.equals("low")) low = guess + 1;
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < secretNumber) {
+                System.out.println("Too low!");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                break;
+            }
         }
+
+        scanner.close();
     }
 }
