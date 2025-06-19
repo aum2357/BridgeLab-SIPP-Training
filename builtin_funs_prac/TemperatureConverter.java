ef40379c5ac9fe39060ddd7b516a9fbd2f363cc5
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-19 11:40:22 +0530

    [Aditya Patel] Add tempreture converter

--- builtin_funs_prac/TemperatureConverter.java
+++ builtin_funs_prac/TemperatureConverter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class TemperatureConverter {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         double temp = sc.nextDouble();
         String type = sc.next();
