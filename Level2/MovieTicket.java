a7b11f2572f86d25e24ce2c83b2741898c2770d9
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-23 12:15:34 +0530

    [Aditya Patel] Add movie ticket booker

--- Level2/MovieTicket.java
+++ Level2/MovieTicket.java
@@ -21,7 +21,7 @@ public class MovieTicket {
         System.out.println("Price: $" + price);
     }
 
-    public static void main(String[] args) {
+    public static void main(String...args) {
         MovieTicket ticket = new MovieTicket("Inception", "A12", 12.50);
         ticket.displayDetails();
         ticket.bookTicket("B15", 15.00);
