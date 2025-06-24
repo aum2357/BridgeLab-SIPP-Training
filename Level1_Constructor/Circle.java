84ae730263e47a26359aee0676f89910431d2150
Aditya Patel <ahambrahmasmi2357@gmail.com>
2025-06-24 11:12:54 +0530

    [Aditya Patel] Add circle demo using constructor

--- Level1_Constructor/Circle.java
+++ Level1_Constructor/Circle.java
@@ -10,7 +10,7 @@ public class Circle {
         this.radius = radius;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Circle c1 = new Circle();
         Circle c2 = new Circle(5.5);
         System.out.println("Default Circle Radius: " + c1.radius);
